3afdf8583197fb824b6b73bcb15cc03076662df2
William Laime <laimewilliam@gmail.com>
2026-03-03 12:59:14 +0100

    implementer devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,7 +12,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "pages#index"
   resources :programs, only: [:index] do
     resources :chats, only: [:index, :show, :new]
   end
--- db/migrate/20260303113715_create_chats.rb
+++ db/migrate/20260303113715_create_chats.rb
@@ -2,8 +2,8 @@ class CreateChats < ActiveRecord::Migration[8.1]
   def change
     create_table :chats do |t|
       t.string :title
-      t.references :program, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+      t.references :program, null: false, foreign_key: true
 
       t.timestamps
     end
