f0b46880a76fb8f9a9e71ef842b5adb00fe862b1
Anatole Ayme <anatole.ayme@gmail.com>
2026-03-05 16:57:27 +0100

    modification de la show des programmes. ajout d'une banner, ajout de cards dans le menu deroulant,retravaille du texte

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,9 +1,14 @@
 class ProgramsController < ApplicationController
   def index
+    redirect_to program_path(Program.first)
   end
 
   def show
-    @program = Program.find(params[:id])
-    @chats = @program.chats.where(user: current_user)
+    @program = Program.find_by(id: params[:id])
+    if @program.nil?
+      redirect_to program_path(Program.first), alert: "Programme introuvable."
+    else
+      @chats = @program.chats.where(user: current_user)
+    end
   end
 end
