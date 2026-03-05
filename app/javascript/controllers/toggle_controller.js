ebc4ef14b5e55ef539bc30d5cfb89907b22a0851
Anatole Ayme <anatole.ayme@gmail.com>
2026-03-05 14:05:47 +0100

    show program retouches

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,14 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["hideable", "show", "mainContent"]
+  static targets = ["hideable", "show", "mainContent", "sidebarCol"]
 
   connect() {
     // Sidebar fermée au chargement
     this.hideableTarget.classList.add("d-none")
     this.showTarget.classList.remove("d-none")
-    this.mainContentTarget.classList.remove("col-md-7")
-    this.mainContentTarget.classList.add("col-md-9")
+    this._collapse()
   }
 
   call(event) {
@@ -20,14 +19,26 @@ export default class extends Controller {
       // 👉 On ouvre la sidebar
       this.hideableTarget.classList.remove("d-none")
       this.showTarget.classList.add("d-none")
-      this.mainContentTarget.classList.remove("col-md-9")
-      this.mainContentTarget.classList.add("col-md-7")
+      this._expand()
     } else {
       // 👉 On ferme la sidebar
       this.hideableTarget.classList.add("d-none")
       this.showTarget.classList.remove("d-none")
-      this.mainContentTarget.classList.remove("col-md-7")
-      this.mainContentTarget.classList.add("col-md-9")
+      this._collapse()
     }
+  }
+
+  _expand() {
+    this.sidebarColTarget.classList.remove("col-auto")
+    this.sidebarColTarget.classList.add("col-md-3")
+    this.mainContentTarget.classList.remove("col")
+    this.mainContentTarget.classList.add("col-md-6")
+  }
+
+  _collapse() {
+    this.sidebarColTarget.classList.remove("col-md-3")
+    this.sidebarColTarget.classList.add("col-auto")
+    this.mainContentTarget.classList.remove("col-md-6")
+    this.mainContentTarget.classList.add("col")
   }
 }
